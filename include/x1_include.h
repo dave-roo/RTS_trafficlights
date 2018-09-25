50451cff13f960ff1696a4c03780b79391bd4efb
Ben Soutter <ben.soutter@hotmail.com>
2018-09-25 17:08:46 +1000

    X1 Workspace added - Ben

---
+++ include/x1_include.h
@@ -0,0 +1,18 @@
+#ifndef _X1_INCLUDE_H_
+#define _X1_INCLUDE_H_
+
+// States for railway crossing
+typedef enum {
+	X1_STATE_0 =0,
+	X1_STATE_1,
+	X1_STATE_2,
+	X1_STATE_3,
+	X1_STATE_4,
+	X1_STATE_5,
+	X1_STATE_6,
+	X1_STATE_7
+	
+} x1_states ;
+
+
+# endif
---
+++ x1/X1_Node/build/x86_64-debug/src/X1_Node.d
@@ -0,0 +1,2 @@
+build/x86_64-debug/src/X1_Node.o: src/X1_Node.c \
+ ../../include/data_types.h ../../include/x1_include.h
