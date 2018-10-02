f7fbaa8197dcd7f493784916290507da6736f960
Jacob de Bruyn <s3433889@student.rmit.edu.au>
2018-10-02 14:03:55 +1000

    client code working

--- intersectionController/src/messageController.c
+++ intersectionController/src/messageController.c
@@ -11,8 +11,6 @@ struct intersection_message *get_All_Sensor()
 {
 
 		intersection_message m = {.train_status = 1, .NE = 1, .SW = 0, .ES = 0, .WN = 0}; // change this struct to test different messages - will need function to really get messages later
-		//turning sensors are within same node
-		//train is external
 
 		mess = &m;
 		return mess;
@@ -28,7 +26,7 @@ int getTrainStatus(char *sname)
 	message_data_t msg;
 	message_data_t reply;
 
-    msg.sending_node = NODE_I1; // set message signal
+    msg.sending_node = NODE_I2; // set message signal
     msg.receiving_node = NODE_X1;
     msg.msg_type = MSG_TRAIN_SIGNAL;
 
@@ -37,28 +35,32 @@ int getTrainStatus(char *sname)
     int index = 0;
     int return_value = -1;
     printf("  ---> Trying to connect to server named: %s\n", sname);
+
+
     if ((server_coid = name_open(sname, 0)) == -1)
     {
         printf("\n    ERROR, could not connect to server!\n\n");
         return_value = -2;//server connect error
     }
+    else {
+		printf("Connection established to: %s\n", sname);
 
-    printf("Connection established to: %s\n", sname);
+		// the data we are sending is in msg.data
+		printf("Client (ID:%d), sending data packet with the integer value: %d \n", msg.sending_node, msg.data);
+		fflush(stdout);
 
-	// the data we are sending is in msg.data
-	printf("Client (ID:%d), sending data packet with the integer value: %d \n", msg.sending_node, msg.data);
-	fflush(stdout);
-
-	if (MsgSend(server_coid, &msg, sizeof(msg), &reply, sizeof(reply)) == -1)
-	{
-		printf(" Error data NOT sent to server\n");
-		return_value = -1; // reply data error
-	}
-	else
-	{ // now process the reply
-		printf("   -->Reply is: '%s'\n", reply.data);
-		return_value = reply.data;
-	}
+		if (MsgSend(server_coid, &msg, sizeof(msg), &reply, sizeof(reply)) == -1)
+		{
+			printf(" Error data NOT sent to server\n");
+			return_value = -1; // reply data error
+		}
+		else
+		{ // now process the reply
+			printf("   -->Reply good\n");
+			printf("   -->Reply is: '%d'\n", reply.data);
+			return_value = reply.data;
+		}
+    }
     name_close(server_coid);
 
     return return_value;
