3794b705d90bab8c22acee42ad222b01098807ba
Ben Soutter <ben.soutter@hotmail.com>
2018-10-03 00:50:19 +1000

    Added error enum

--- include/global_include.h
+++ include/global_include.h
@@ -1,19 +1,35 @@
 // Header file for common data types
-
 #ifndef _DATA_TYPE_H
 #define _DATA_TYPE_H
 #include <stdint.h>
+
+// Message passing
+#include <errno.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+#include <fcntl.h>
+#include <share.h>
+
+#define X1_QNET_ATTACH_POINT "/net/s3484378-01/dev/name/local/x1_group_14" // Need to update to match node hostname
 
 // Message types for data between nodes
 typedef enum{
     MSG_TRAIN_SIGNAL = 0,
 	MSG_CURRENT_STATE,
 	MSG_CONTROL_STATE_LOCK,
-	MSG_CONTROL_STATE_RELEASE
+	MSG_CONTROL_STATE_RELEASE,
+	MSG_ERROR
     // Any others that need to be added here
 } message_type;
 
+// Connection errors that can be returned from function
+typedef enum{
+	MSG_CONNECTION_ERROR,
+	MSG_SENDING_ERROR
+	// Any Other error types here
+} message_error;
 
+// All nodes that will be sending/receiving data
 typedef enum{
     NODE_I1 = 0,
     NODE_I2,
@@ -21,8 +37,11 @@ typedef enum{
     NODE_CONTROLLER
 } node_id;
 
+// This struct is what gets passed in the message
 typedef struct{
+	struct _pulse hdr; // This is a system header required for message passing
     node_id sending_node;
+    node_id receiving_node;
     message_type msg_type; 
     uint8_t data;
 } message_data_t;
--- include/x1_include.h
+++ include/x1_include.h
@@ -1,9 +1,13 @@
 #ifndef _X1_INCLUDE_H_
 #define _X1_INCLUDE_H_
+#include <stdio.h>
+#include <pthread.h>
 #include <semaphore.h>
 
+#define ATTACH_POINT "x1_group_14"
+
 // States for railway crossing
-typedef enum {
+typedef enum{
 	X1_STATE_0 =0,
 	X1_STATE_1,
 	X1_STATE_2,
@@ -14,6 +18,7 @@ typedef enum {
 	X1_STATE_7
 } x1_states ;
 
+// Signal Types for X1
 typedef enum{
 	X1_SIGNAL_NONE = 0,
 	X1_SIGNAL_Ein,
@@ -23,13 +28,20 @@ typedef enum{
 	X1_SIGNAL_BoomGateDown
 } x1_signals ;
 
-// Struct to pass global data
+// Priority struct
+struct sm_priority_t{
+	x1_states last_state; // This is used for priority traffic
+	uint8_t set;
+	uint8_t x1_reset; // This is used as a conditional variable for the state machine
+};
+
+// Struct for global data
 typedef struct{
 	x1_states current_state;
 	uint8_t sensor_received;
 	uint8_t signal;
 	sem_t sem;
+	struct sm_priority_t priority;
 } sm_data_t;
-
 
 # endif
